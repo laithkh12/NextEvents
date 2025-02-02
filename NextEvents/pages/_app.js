dae91d4793c18a906576e121253dcf87f2c9c6a7
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-02-02 22:11:39 +0200

    adding head and meta

--- NextEvents/pages/_app.js
+++ NextEvents/pages/_app.js
@@ -1,12 +1,20 @@
-import Layout from '../components/layout/layout'
-import '../styles/globals.css'
+// Root app component
+
+import Head from 'next/head';
+import Layout from '../components/layout/layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return(
+  return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events'/>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+      </Head>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
