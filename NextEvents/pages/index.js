bdf43a07cbd323b66bb1706efd5d296c57647d8a
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-02-02 22:13:16 +0200

    Update index.js

--- NextEvents/pages/index.js
+++ NextEvents/pages/index.js
@@ -1,13 +1,28 @@
-import EventList from '../components/events/event-list'
-import { getFeaturedEvents } from '../dummy-data'
+import Head from 'next/head';
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
 
-function HomePage() {
-    const featuredEvents = getFeaturedEvents()
-    return(
-        <div>
-            <EventList items={featuredEvents}/>
-        </div>
-    )
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta name='description' content='Find a lot of great events that allow you to evolve...'/>
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
 }
 
-export default HomePage
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    },
+    revalidate: 1800
+  }
+}
+
+export default HomePage;
