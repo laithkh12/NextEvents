0687aa330132160a018e60e91d8c4278ed8a9733
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-02-06 00:34:40 +0200

    Update _app.js

--- NextEvents/pages/_app.js
+++ NextEvents/pages/_app.js
@@ -1,6 +1,5 @@
-// Root app component
-
 import Head from 'next/head';
+
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
@@ -9,8 +8,8 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>Next Events</title>
-        <meta name='description' content='NextJS Events'/>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
     </Layout>
