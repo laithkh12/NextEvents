697c18192359d37750aaa71e67835aaf4bc86c11
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-02-06 00:31:51 +0200

    Update layout.js

--- NextEvents/components/layout/layout.js
+++ NextEvents/components/layout/layout.js
@@ -1,15 +1,14 @@
-import { Fragment } from "react"
-import MainHeader from "./main-header"
+import { Fragment } from 'react';
 
-function Layout(props){
-    return(
-        <Fragment>
-            <MainHeader/>
-            <main>
-                {props.children}
-            </main>
-        </Fragment>
-    )
+import MainHeader from './main-header';
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+    </Fragment>
+  );
 }
 
-export default Layout
+export default Layout;
