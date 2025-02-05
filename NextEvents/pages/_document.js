d22855682146db42f8a1a2c294a7f270f815694d
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-02-06 00:34:49 +0200

    Update _document.js

--- NextEvents/pages/_document.js
+++ NextEvents/pages/_document.js
@@ -1,18 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDuocument extends Document{
-    render(){
-        return(
-            <Html lang="en">
-                <Head />
-                <body>
-                    <div id="overlays"/>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <div id='overlays' />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDuocument
+export default MyDocument;
