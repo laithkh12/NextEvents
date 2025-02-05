6a9e44daec315cd7f1bce8933bdb0a203134884e
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-02-06 00:29:43 +0200

    Update event-item.js

--- NextEvents/components/events/event-item.js
+++ NextEvents/components/events/event-item.js
@@ -1,9 +1,10 @@
+import Image from 'next/image';
+
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './event-item.module.css';
-import Image from 'next/image';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -18,7 +19,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <Image src={'/' + image} alt={title} width={250} height={160}/>
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
